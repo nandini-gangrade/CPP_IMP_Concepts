16cf67561bf22bbb510403f7f54cbb52f661c0a7
Nandini Gangrade <gangradeenandinii@gmail.com>
2022-06-25 11:36:10 +0530

    Modified Arrays (Removed someones code)

--- cpp_programs/1-arrays.cpp
+++ cpp_programs/1-arrays.cpp
@@ -33,12 +33,3 @@ using namespace std;
 //     }
 //     return 0;
 // }
-
-//Mark
-// int main(){
-//     int A[5]={1,2,3,4,5};
-//     for(int x:A){
-//         cout<<x<<endl;
-//     }
-//     return 0;
-// }
